51b963bf0fd8e0000fb591c406efa3707498b5ed
zaqaz113 <zaqaz113@naver.com>
2023-03-14 17:22:48 +0900

    idea post detail

---
+++ src/page/Posts/idea_list.js
@@ -0,0 +1,68 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { setCookie, getCookie, removeCookie } from '../../util/Cookie';
+import { useNavigate } from "react-router";
+
+import { Card } from 'react-bootstrap'
+
+import './idea_list.css'
+
+
+function Idealist() {
+  const [idea, setIdea] = useState([]);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_KEY}/idea/list`, {
+        headers: {
+          Authorization: getCookie("token"),
+        },
+      })
+      .then((result) => {
+        const statusCode = result.status;
+        if (statusCode === 201) {
+          const ideas = result.data.data;
+          setIdea(ideas);
+        }
+      })
+      .catch((err) => {
+        const statusCode = err.status;
+        if (statusCode === 400) {
+          alert("토큰 에러")
+        }
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mt-5">
+            <h2>아이디어 목록</h2>
+          </div>
+              {idea.map((item,i) => (
+                <div className="col-4 idea-wrap">
+                  <Card style={{ width: '18rem' ,height: '18rem'}}>
+                    <Card.Body>
+                      <Card.Title>{item.title}</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                      <Card.Text>
+                        생성일:{item.first_date}<br/>
+                        수정일:{item.update_date}
+                      </Card.Text>
+                      <Card.Link onClick={() => {
+                        navigate(`/idea_list/${item._id}`, { state: { id: item._id  } })
+                      }}>아이디어 보러가기</Card.Link>
+                      <Card.Link href="#">{item.creator}</Card.Link>
+                    </Card.Body>
+                  </Card>
+                </div>
+              ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Idealist 
