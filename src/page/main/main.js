d548bdc6905c6a6e2a31dfd865895741490c5a98
zaqaz113 <zaqaz113@naver.com>
2023-03-25 19:27:30 +0900

    main page fix 로그인 유무에 따른 페이지 제한

--- src/page/main/main.js
+++ src/page/main/main.js
@@ -12,23 +12,38 @@ function MainPage() {
         <div className="container">
           <div className="row main-wrap">
             {/* main desc left */}
-            <div className="col-md-8">
+            <div className="col-md-6">
               <h5 className='main-wrap-header'>
                 idea Connection
               </h5>
               <h1 className='main-wrap-headline'>
-                Anyone can<br/> implement an<br/> idea
+                Anyone can<br /> implement an<br /> idea
               </h1>
               <div className='main-wrap-btns'>
-                <button className='main-btns-join' onClick={() => { navigate('/idea_list')}}>Idea List</button>
+                <button className='main-btns-join' onClick={() => { navigate('/idea_list') }}>Idea List</button>
                 <button className='main-btns-learn' onClick={() => { navigate('/introduce') }}>Learn More</button>
               </div>
             </div>
             {/* main desc left end */}
-  
+
             {/* main img */}
-            <div className="col-md-4">
-              <div className='main-img'>
+            <div className="col-md-6">
+              <div className='main-img-wrap'>
+                <div className='main-img-border1'>
+                  <div className='border1-circle'></div>
+                  <img src={process.env.PUBLIC_URL + '/img/portrait.jpg'} alt='logo image' className='border1-circle-img' />
+                  <div className='main-img-border2'>
+                    <img src={process.env.PUBLIC_URL + '/img/hijab.jpg'} alt='logo image' className='border2-circle-img' />
+                    <div className='main-img-border3'>
+                      <div className='border3-circle1'></div>
+                      <div className='border3-circle2'></div>
+                      <img src={process.env.PUBLIC_URL + '/img/girl.jpg'} alt='logo image' className='border3-circle-img' />
+                      <div className='main-img-border4'>
+                        <img src={process.env.PUBLIC_URL + '/img/happy.jpg'} alt='logo image' className='center-img' />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             {/* main img end */}
@@ -39,10 +54,10 @@ function MainPage() {
   }
   return (
     <>
-      <div className="container">
+      <div className="container main">
         <div className="row main-wrap">
           {/* main desc left */}
-          <div className="col-md-8">
+          <div className="col-md-6">
             <h5 className='main-wrap-header'>
               idea Connection
             </h5>
@@ -57,8 +72,23 @@ function MainPage() {
           {/* main desc left end */}
 
           {/* main img */}
-          <div className="col-md-4">
-            <div className='main-img'>
+          <div className="col-md-6">
+            <div className='main-img-wrap'>
+              <div className='main-img-border1'>
+                <div className='border1-circle'></div>
+                <img src={process.env.PUBLIC_URL + '/img/portrait.jpg'} alt='logo image' className='border1-circle-img' />
+                <div className='main-img-border2'>
+                  <img src={process.env.PUBLIC_URL + '/img/hijab.jpg'} alt='logo image' className='border2-circle-img' />
+                  <div className='main-img-border3'>
+                    <div className='border3-circle1'></div>
+                    <div className='border3-circle2'></div>
+                    <img src={process.env.PUBLIC_URL + '/img/girl.jpg'} alt='logo image' className='border3-circle-img' />
+                    <div className='main-img-border4'>
+                      <img src={process.env.PUBLIC_URL + '/img/happy.jpg'} alt='logo image' className='center-img' />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           {/* main img end */}
